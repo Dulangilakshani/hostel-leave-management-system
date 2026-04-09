01275af1acb14b02f21e57aa0c39bddfecbc93be
Dulangi Thennakoon <167986364+Dulangilakshani@users.noreply.github.com>
2026-04-09 15:06:54 +0530

    Update leaveRoutes.js

--- server/routes/leaveRoutes.js
+++ server/routes/leaveRoutes.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const { createLeave, returnLeave, getMyLeaves } = require("../controllers/LeaveController");
-const { protect } = require("../middleware/AuthMiddleware");
 
-router.post("/create", protect, createLeave);
-router.put("/return", protect, returnLeave);
-router.get("/my-history", protect, getMyLeaves);
+const {
+  createLeave,
+  returnLeave,
+  getMyLeaves,
+  getCurrentOut,
+  getLateReturns,
+  getAllLeaves,
+} = require("../controllers/LeaveController");
+
+const {
+  protect,
+  isWarden,
+  isStudent,
+} = require("../middleware/AuthMiddleware");
+
+// Student routes
+router.post("/create", protect, isStudent, createLeave);
+router.put("/return", protect, isStudent, returnLeave);
+router.get("/my-history", protect, isStudent, getMyLeaves);
+
+// Warden routes
+router.get("/current-out", protect, isWarden, getCurrentOut);
+router.get("/late", protect, isWarden, getLateReturns);
+router.get("/all", protect, isWarden, getAllLeaves);
 
 module.exports = router;
