8a3f125a59ee0173ac181dfee44058af70f4ec2d
Dulangi Thennakoon <167986364+Dulangilakshani@users.noreply.github.com>
2026-04-09 15:28:35 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -12,4 +12,15 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+API.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.clear();
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default API;
