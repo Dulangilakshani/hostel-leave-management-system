bc28b4c54228c01f4e17ed5775e827d4c9874623
Dulangi Thennakoon <167986364+Dulangilakshani@users.noreply.github.com>
2026-04-09 18:47:05 +0530

    Update AuthRoutes.js

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/AuthController");
+const {
+  registerUser,
+  loginUser,
+  resetPassword,
+} = require("../controllers/AuthController");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/reset-password", resetPassword);
 
 module.exports = router;
